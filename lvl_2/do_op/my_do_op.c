b861ab7f54a71643435a5fab67a2545d20293d9e
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-05 16:00:09 +0200

    dooone

---
+++ lvl_2/do_op/my_do_op.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_do_op.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-la-f <nde-la-f@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 15:20:18 by nde-la-f          #+#    #+#             */
+/*   Updated: 2023/09/05 16:00:01 by nde-la-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	main(int ac, char *av[])
+{
+	if (ac == 4)
+	{
+		if (av[2][0] == '+')
+			printf("%d", (atoi(av[1]) + atoi(av[3])));
+		else if (av[2][0] == '-')
+			printf("%d", (atoi(av[1]) - atoi(av[3])));
+		else if (av[2][0] == '*')
+			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		else if (av[2][0] ==  '/')
+			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		else if (av[2][0] == '%')
+			printf("%d", (atoi(av[1]) % atoi(av[3])));
+	}
+	printf("\n");
+	return (0);
+}
+
