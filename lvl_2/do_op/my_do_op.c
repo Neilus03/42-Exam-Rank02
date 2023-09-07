f7111f2aab55f8eb3b3118704e5270a7f2bee764
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-09-07 15:36:18 +0200

    done

--- lvl_2/do_op/my_do_op.c
+++ lvl_2/do_op/my_do_op.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   my_do_op.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nde-la-f <nde-la-f@student.42barcel>       +#+  +:+       +#+        */
+/*   By: nde-la-f <nde-la-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:20:18 by nde-la-f          #+#    #+#             */
-/*   Updated: 2023/09/05 16:00:01 by nde-la-f         ###   ########.fr       */
+/*   Updated: 2023/09/07 15:36:18 by nde-la-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int	main(int ac, char *av[])
 {
@@ -24,12 +24,11 @@ int	main(int ac, char *av[])
 			printf("%d", (atoi(av[1]) - atoi(av[3])));
 		else if (av[2][0] == '*')
 			printf("%d", (atoi(av[1]) * atoi(av[3])));
-		else if (av[2][0] ==  '/')
-			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		else if (av[2][0] == '/')
+			printf("%d", (atoi(av[1]) / atoi(av[3])));
 		else if (av[2][0] == '%')
 			printf("%d", (atoi(av[1]) % atoi(av[3])));
 	}
 	printf("\n");
 	return (0);
 }
-
